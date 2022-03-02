de5aba4cbb45dc4df78365b26e10ba841b01666a
zhezheb <100422048+zhezheb@users.noreply.github.com>
2022-03-02 21:34:48 +0800

    Add files via upload

---
+++ test3_2.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+//int teest(int x)
+//{
+//	if (x % 4 == 0 && x % 400 != 0 || x %400==0)
+//		return 1;
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int input = 0;
+//	scanf_s("%d", &input);
+//	int len= teest(input);
+//	if (len == 1)
+//	{
+//		printf("is run nian");
+//	}
+//	else
+//	{
+//		printf("not is");
+//	}
+//
+//	return 0;
+//}
+//
+//int fact(int x)
+//{
+//	int i = 0;
+//	int sum = 1;
+//	for (i = 1; i <= x; i++)
+//	{
+//		sum = i * sum;
+//	}return sum;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		printf("%-4d%-20d", i, fact(i));
+//		printf("\n");
+//	}
+//	return 0;
+//}
