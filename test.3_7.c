0ed04b7818b40007a1ede666def67a397c00ddcc
zhezheb <100422048+zhezheb@users.noreply.github.com>
2022-03-09 23:25:09 +0800

    Add files via upload

--- test.3_7.c
+++ test.3_7.c
@@ -28,23 +28,78 @@
 //	}
 //	return 0;//                  1076
 //}
+//int main()
+//{
+//	int t;
+//	scanf_s("%d", &t);
+//	int arr[100] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < t; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (i = 0; i < t; i++)
+//	{
+//		for (int j = 1; j < arr[i]; j++)
+//		{
+//			if(j)
+//		}
+//	}
+//	        
+//	return 0;
+//}
+//void del(int arr[], int n, int m)
+//{
+//	int j = 0;
+//	for (j = m; j < n-1; j++)
+//	{
+//		arr[j] = arr[j + 1];
+//	}
+//}
+//void print(int arr[], int n)
+//{
+//	int i = 0;
+//	for (i = 0; i < n - 1; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//int main()
+//{
+//	int n;
+//	scanf_s("%d", &n);
+//	int arr[100] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf_s("%d", &arr[i]);
+//	}
+//	int m = 0;
+//	scanf_s("%d", &m);
+//	del(arr, n, m);
+//	print(arr, n);//删除特定元素
+//	return 0;
+//}
+//#include<window.h>
 int main()
 {
-	int t;
-	scanf_s("%d", &t);
-	int arr[100] = { 0 };
-	int i = 0;
-	for (i = 0; i < t; i++)
+again:	printf("你是大傻币吗：1(yes)/0(no)");
+	int input;
+	scanf_s("%d", &input);
+	if (input == 1)
 	{
-		scanf("%d", &arr[i]);
+		printf("恭喜你，答对了");
 	}
-	for (i = 0; i < t; i++)
+	else if (input == 0)
 	{
-		for (int j = 1; j < arr[i]; j++)
-		{
-			if(j)
-		}
+		printf("000");
 	}
-	        
+	else
+	{
+		printf("有够沙的");
+		goto again;
+	
+	}
 	return 0;
+
 }
